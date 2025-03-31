67cc429f1aa8f82bb1ce9572f0a48d100077fd4d
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-03-31 13:55:07 +0200

    booking class

--- DSVBooking/Repository/BookCollectionRepository.cs
+++ DSVBooking/Repository/BookCollectionRepository.cs
@@ -4,19 +4,39 @@ namespace DSVBooking.Repository
 {
     public class BookCollectionRepository : IBookRepository
     {
-        List<Booking> _bookings = new List<Booking>();
+        private List<Booking> _bookings;
+
+        public BookCollectionRepository()
+        {
+            _bookings = new List<Booking>();
+            Seed();
+        }
 
         /// <summary>
-        /// Return af copy of the list, so it can't be changed from the outside
+        /// adds a new booking to the repository
         /// </summary>
-        /// <returns></returns>
+        /// <param name="booking">the booking being added</param>
+        public void Add(Booking booking)
+        {
+            _bookings.Add(booking);
+        }
+
+        /// <summary>
+        /// Henter alle kæledyr i repositoryet.
+        /// Dette svarer til en "Read all"-operation i en database.
+        /// </summary>
+        /// <returns>En liste med alle kæledyr.</returns>
         public List<Booking> GetAll()
         {
             return _bookings;
         }
-
-        
-
+        private void Seed()
+        {
+            _bookings.Add(new Booking(1, , 17/07/2025, 18/07/2025, "we want cookies!"));
+            _bookings.Add(new Booking());
+            _bookings.Add(new Booking());
+        }
+        /*
         /// <summary>
         /// Iterate through the list and find a specefic id
         /// </summary>
@@ -29,6 +49,6 @@ namespace DSVBooking.Repository
                 return booking;
             }
             return null;
-        }
+        }*/
     }
 }
