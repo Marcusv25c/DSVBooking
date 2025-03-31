dbd89934b73a3df619180f95b9d87ed79c2d0edc
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-03-31 13:27:39 +0200

    Room fix

--- DSVBooking/Model/Room.cs
+++ DSVBooking/Model/Room.cs
@@ -4,19 +4,20 @@ namespace DSVBooking.Model
 {
     public class Room
     {
+        private static int _tempID = 1; // instance
+
         public string Name { get; set; }
         public int ID { get; set; }  
         public int Capacity { get; set; }
         public bool Projector { get; set; }
         public bool Whiteboard { get; set; }
         public bool SmartBoard { get; set; }
-
-        
+                
 
         public Room()
         {
             Name = "test";
-            ID++;
+            ID = _tempID++;
             Capacity = 0;
             Projector = false;
             Whiteboard = false;
@@ -34,7 +35,6 @@ namespace DSVBooking.Model
         // In depth Constructor with all proterties
         public Room(string name, int cap, bool projector, bool whiteboard, bool smartboard) : this( name,  cap)
         {
-          
             Projector = projector;
             Whiteboard = whiteboard;
             SmartBoard = smartboard;
