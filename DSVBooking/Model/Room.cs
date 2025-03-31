694bbbb30f4b9e4edbaa5cea2d84d74a3990f58d
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-03-31 11:14:57 +0200

    Room update

--- DSVBooking/Model/Room.cs
+++ DSVBooking/Model/Room.cs
@@ -5,12 +5,13 @@ namespace DSVBooking.Model
     public class Room
     {
         public string Name { get; set; }
-        static int ID { get; set; }
+        static int ID { get; set; } // Static for easy constructing 
         public int Capacity { get; set; }
         public bool Projector { get; set; }
         public bool Whiteboard { get; set; }
         public bool SmartBoard { get; set; }
 
+        // Default Contructor for a room without equipment
         public Room(string name, int cap)
         {
             Name = name;
@@ -21,6 +22,7 @@ namespace DSVBooking.Model
             SmartBoard = false;
         }
 
+        // In depth Constructor with all proterties
         public Room (string name, int cap, bool projector, bool whiteboard, bool smartboard)
         {
             Name = name;
