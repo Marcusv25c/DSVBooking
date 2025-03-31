5f44c40f6f931e47d01d43a76f282ef76821410a
Markus Viktor Lynggaard <mal024@edu.zealand.dk>
2025-03-31 11:44:35 +0200

    Collection Booking

--- DSVBooking/Repository/BookCollectionRepository.cs
+++ DSVBooking/Repository/BookCollectionRepository.cs
@@ -1,6 +1,34 @@
-﻿namespace DSVBooking.Repository
+﻿using DSVBooking.Model;
+using System.Reflection.Metadata.Ecma335;
+namespace DSVBooking.Repository
 {
-    public class BookCollectionRepository
+    public class BookCollectionRepository : IBookRepository
     {
+        List<Booking> _bookings = new List<Booking>();
+
+        /// <summary>
+        /// Return af copy of the list, so it can't be changed from the outside
+        /// </summary>
+        /// <returns></returns>
+        public List<Booking> GetAll()
+        {
+            return _bookings;
+        }
+
+        
+
+        /// <summary>
+        /// Iterate through the list and find a specefic id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Booking Get(int id)
+        {
+            foreach(Booking booking in _bookings)
+            {
+                return booking;
+            }
+            return null;
+        }
     }
 }
