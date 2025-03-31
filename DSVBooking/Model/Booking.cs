35d11734e4cd95acf823ed61729749cca248a075
Egil Bukhave Edström <ege001@edu.zealand.dk>
2025-03-31 11:33:55 +0200

    added id to booking class

--- DSVBooking/Model/Booking.cs
+++ DSVBooking/Model/Booking.cs
@@ -3,12 +3,16 @@
     public class Booking
     {
         /// <summary>
-        /// Booking class describes the currently booked rooms. It uses the Room class, starting datetime, end datetime of booking and the user's written comment as properties.
+        /// Booking class describes the currently booked rooms. 
+        /// It uses the Room class
+        /// starting datetime
+        /// end datetime of booking and the user's written comment as properties
         /// </summary>
         Room _room;
         DateTime _startDateTime;
         DateTime _endDateTime;
         string _comment;
+        int _iD;
 
 
     /// <summary>
@@ -16,24 +20,27 @@
     /// StartDateTime is the starting time and date for the booking.
     /// EndDateTime describes when the booking ends.
     /// The comment is a user input which contains information about the booking.
+    /// The iD is a unique identifier for the user's specific.
     /// </summary>
         public Room Room { get => _room; set => _room = value; }
         public DateTime StartDateTime { get => _startDateTime; set => _startDateTime = value; }
         public DateTime EndDateTime { get => _endDateTime; set => _endDateTime = value; }
         public string Comment { get => _comment; set => _comment = value; }
+        public int ID { get => _iD; set => _iD = value; }
 
         /// <summary>
-        /// Room represents the room that is being booked. 
         /// StartDateTime is the starting time and date for the booking.
         /// EndDateTime describes when the booking ends.
         /// The comment is a user input which contains information about the booking.
         /// </summary>
-        /// <param name="room"></param>
-        /// <param name="startDateTime"></param>
-        /// <param name="endDateTime"></param>
-        /// <param name="comment"></param>
-        Booking(Room room, DateTime startDateTime, DateTime endDateTime, string comment)
+        /// <param name="iD">Unique identifier for booking</param>
+        /// <param name="room">Name of the room in the building</param>
+        /// <param name="startDateTime">Start time of booking</param>
+        /// <param name="endDateTime">End time of booking</param>
+        /// <param name="comment">User input comment about booking</param>
+        Booking(int iD, Room room, DateTime startDateTime, DateTime endDateTime, string comment)
         {
+            _iD = iD;
             _room = room;
             _startDateTime = startDateTime;
             _endDateTime = endDateTime;
