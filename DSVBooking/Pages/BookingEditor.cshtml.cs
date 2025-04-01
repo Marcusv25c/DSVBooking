3f3033de7c561b2385211e9555ef1a721e3af3a3
Marcus Rasmussen <mar017@edu.zealand.dk>
2025-04-01 11:54:06 +0200

    Booking moved from Index to BE

--- DSVBooking/Pages/BookingEditor.cshtml.cs
+++ DSVBooking/Pages/BookingEditor.cshtml.cs
@@ -1,10 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using DSVBooking.Services;
+using DSVBooking.Repository;
+using DSVBooking.Model;
+
 
 namespace DSVBooking.Pages
 {
     public class BookingEditorModel : PageModel
     {
+        private readonly RoomService _rs;
+        private readonly BookService _bs;
+        [BindProperty]
+        public List<Room> Rooms { get; set; }
+        public List<Booking> Bookings { get; set; }
+
+        public BookingEditorModel(BookService bs, RoomService rs)
+        {
+            Bookings = bs.GetAll();
+            Rooms = rs.GetAll();
+            _bs = bs;
+            _rs = rs;
+        }
         public void OnGet()
         {
         }
